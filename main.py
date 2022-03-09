0e5c3898fd20565d9bce838e700b08303fba5cd1
Flechaa7 <91721888+joseluis031@users.noreply.github.com>
2022-03-09 16:57:22 +0000

    main

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ if __name__=="__main__:
 
 v = int(input("Elija el ejercicio que desea ejecutar(1-3): "))
 if v == 1:
-  from class import dicotomia
+  from trabajo import dicotomia
 elif v == 2:
-  from class import topologia
+  from trabajo import topologia
 elif v == 3:
-  from class import especificaciones
+  from trabajo import especificaciones
 else:
   print("Introduzcaun numero del 1 al 3 por favor:")
