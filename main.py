cf7b4e8444ccd74e7516b9c0239293f79b8a3336
Flechaa7 <91721888+joseluis031@users.noreply.github.com>
2022-03-09 16:55:51 +0000

    main listo

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from dicotomia import *
 
-from divideyvenceras import *
+from topologia import *
 
-if __name__=="__main__":
+from especificaciones import *
+
+if __name__=="__main__:
+
+v = int(input("Elija el ejercicio que desea ejecutar(1-3): "))
+if v == 1:
+  from class import dicotomia
+elif v == 2:
+  from class import topologia
+elif v == 3:
+  from class import especificaciones
+else:
+  print("Introduzcaun numero del 1 al 3 por favor:")
