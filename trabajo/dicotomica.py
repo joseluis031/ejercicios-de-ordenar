a8d10165b6388da89dfc6c8e9bfa88a0114e419b
Flechaa7 <91721888+joseluis031@users.noreply.github.com>
2022-03-12 11:24:21 +0000

    ejercicio dicotomia

--- trabajo/dicotomica.py
+++ trabajo/dicotomica.py
@@ -1,3 +1,26 @@
 class dicotomia:
-  def enumeracion(self,1,2,3):
-     
+  def __init__(self,tabla):
+     self.tabla = tabla
+
+  def ordenar(self):
+    self.tabla.sort()
+    return self.tabla
+
+class dicot:
+  def __init__(self,tabla):
+    dicotomia.__init__(self,tabla)
+    l = []
+    self.l = l
+  def ordenar_lista(self):
+    dicotomia.ordenar(self)
+    for i in self.tabla:
+      self.l.append(i)
+    return self.l
+
+if __name__ == "__main__":
+  tabla = [3,5,1,8,9,2,0,7,6,4]
+  print(tabla)
+  resultado = dicotomia(tabla)
+  print("La tabla ordenada es: {}".format(resultado.ordenar()))
+  final = dicot(tabla)
+  print ("La tabla ordenada a partir de una lista vacia es: {}".format(final.ordenar_lista()))
