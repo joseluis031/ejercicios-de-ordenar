c349c5e22bede33e0c958621d791d0e0dfb14ef6
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-03-13 21:56:47 +0100

    corregido el fallo

--- Trabajo/topologia.py
+++ Trabajo/topologia.py
@@ -6,10 +6,10 @@ class Lista:
             for j in range(len(self.L)-1):
                 if self.L[j]>self.L[j+1]:
                    print("Intercambio {} por {} ".format(self.L[j], self.L[j+1]))
-                   self.L[j],self.L[j+1],self.L[j]
+                   self.L[j],self.L[j+1]=self.L[j+1],self.L[j]
         return self.L
     
-if __name__ == "__main__":  #no consigo que se ordene la lista
+if __name__ == "__main__":  
     L = [3,14,1,9,6,5,8,10,2]
     resultado = Lista(L)
     print ("El resultado es {}".format(resultado.ordenacion()))
