5b39ff414872a214d7607dfc531298dc58173c59
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-03-13 17:46:31 +0100

    topologia

--- Trabajo/topologia.py
+++ Trabajo/topologia.py
@@ -1,11 +1,15 @@
 class Lista:
-  def __init__(self,lista):
-    self.lista = lista
-  def ordenacion(self):
-    for i in range(len(self.lista)-1):
-      for j in range(len(self.lista)-1):
-        if self.lista[j]>self.lista[j+1]:
-          print("Intercambio {} por {} ".format(self.lista[j], self.lista[j+1]))
-          self.lista[j],self.lista[j+1],self.lista[j]
-    return self.lista
-          
+    def __init__(self,L):
+        self.L = L
+    def ordenacion(self):
+        for i in range(len(self.L)-1):
+            for j in range(len(self.L)-1):
+                if self.L[j]>self.L[j+1]:
+                   print("Intercambio {} por {} ".format(self.L[j], self.L[j+1]))
+                   self.L[j],self.L[j+1],self.L[j]
+        return self.L
+    
+if __name__ == "__main__":  
+    L = [3,14,1,9,6,5,8,10,2]
+    resultado = Lista(L)
+    print ("El resultado es {}".format(resultado.ordenacion(L)))
--- main.py
+++ main.py
@@ -1,25 +1,24 @@
-from dicotomica import *
+from Trabajo.dicotomica import *
 
-from topologia import *
+from Trabajo.topologia import *
 
-from especificaciones import *
+from Trabajo.especificaciones import *
 
 if __name__=="__main__":
 
-from Trabajo.dicotomica import dicotomia, dicot
-print("Ejercicio 1a")
-tabla = [3,5,1,8,9,2,0,7,6,4]
-print(tabla)
-resultado = dicotomia(tabla)
+  from Trabajo.dicotomica import dicotomia, dicot
+  print("Ejercicio 1a")
+  tabla = [3,5,1,8,9,2,0,7,6,4]
+  print(tabla)
+  resultado = dicotomia(tabla)
   print("La tabla ordenada es: {}".format(resultado.ordenar()))
 
-print("Ejercico 1b")
-final = dicot(tabla)
+  print("Ejercico 1b")
+  final = dicot(tabla)
   print ("La tabla ordenada a partir de una lista vacia es {}".format(final.ordenar_lista()))
 
-from Trabajo.topologia import Lista
-    print ("Ejercicio 2")
-    vector = [3,14,1,9,6,5,8,10,2]
-    resultado = Lista(vector)
-    #Mostramos el resultado por pantalla
-    print ("El resultado es {}".format(resultado.ordenacion()))
+  from Trabajo.topologia import Lista
+  print ("Ejercicio 2")
+  L = [3,14,1,9,6,5,8,10,2]
+  resultado = Lista(L)
+  print ("El resultado es {}".format(resultado.ordenacion(L)))
